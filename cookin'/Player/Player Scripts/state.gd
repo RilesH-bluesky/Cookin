17519822e338a5047e015b0c1de901e2a0076e02
NathKoz <125074117+NathKoz@users.noreply.github.com>
2025-04-26 09:25:15 -0400

    Working Base Version 1.0.0

---
+++ cookin'/Player/Player Scripts/state.gd
@@ -0,0 +1,25 @@
+class_name State extends Node
+
+static var player: Player
+
+func _ready():
+	pass
+
+## What happens when the player enters this state?
+func Enter() -> void:
+	pass
+
+func Exit() -> void:
+	pass
+
+func Process( _delta : float ) -> State:
+	return null
+	pass
+	
+func Physics (_delta : float ) -> State:
+	return null
+	pass
+
+func HandleInput( _event: InputEvent ) -> State:
+	return null
+	pass
